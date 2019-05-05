dd8c0f9844d7823f2faab3dfed81f415a9fef96c
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-05-05 01:10:30 -0400

    Beginnings of the html

--- src/com/webminesweeper/webminesweeper.py
+++ src/com/webminesweeper/webminesweeper.py
@@ -1,13 +1,11 @@
 from src.com.webminesweeper.model.Board import Board
-from src.com.webminesweeper.model.Tile import Tile
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
-def main():
+@app.route('/game')
+def game():
 
     board = Board(5, 5)
     board.place_bombs(4)
-    print(board.__repr__())
-
-
-if __name__ == "__main__":
-    main()
+    return render_template('game.html', board=board)
